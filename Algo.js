6f8f09ae2e98666ff37d096d4dac107092d73977
o3junior <156916101+o3junior@users.noreply.github.com>
2024-08-09 16:08:22 +0530

    Add files via upload

---
+++ Algo.js
@@ -0,0 +1,8 @@
+function Specify()
+{
+    var Machines = document.getElementById("machine").value;
+    var Component = document.getElementById("component").value;
+    var Parameter = document.getElementById("parameter").value;
+    var Value = parseFloat(document.getElementById("value").value);
+    document.getElementById("result").innerHTML = "Better Luck Next Time";
+}
